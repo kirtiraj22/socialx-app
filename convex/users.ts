33e9b7bca020abbc76b2cbbd5f74ff60a7bd7269
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-04-19 15:16:33 +0530

    db: create user mutation

---
+++ convex/users.ts
@@ -0,0 +1,34 @@
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+
+export const createUser = mutation({
+	args: {
+		username: v.string(),
+		fullname: v.string(),
+		email: v.string(),
+		bio: v.optional(v.string()),
+		image: v.optional(v.string()),
+		clerkId: v.string(),
+	},
+	handler: async (ctx, args) => {
+		const existingUser = await ctx.db
+			.query("users")
+			.withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+			.first();
+
+		if (existingUser) return;
+
+		await ctx.db.insert("users", {
+			username: args.username,
+			fullname: args.fullname,
+			bio: args.bio,
+			email: args.email,
+			image: args.image,
+			clerkId: args.clerkId,
+			followers: 0,
+			following: 0,
+			posts: 0,
+		});
+	},
+});
