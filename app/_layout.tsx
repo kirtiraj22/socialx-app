44deee378d72efe61de1ff3af477641b6ac72234
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-04-19 11:01:44 +0530

    feat: clerk(auth) and convex(db) setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,15 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import InitialLayout from "@/components/initialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
 	return (
-		<ClerkProvider tokenCache={tokenCache}>
+		<ClerkAndConvexProvider>
 			<SafeAreaProvider>
 				<SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
 					<InitialLayout />
 				</SafeAreaView>
 			</SafeAreaProvider>
-		</ClerkProvider>
+		</ClerkAndConvexProvider>
 	);
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://champion-alien-25.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
